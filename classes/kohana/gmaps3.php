f8ba8e2136cec7b2b7ac5bdc61be2eb168e594ec
Juan Lago <juanparati@gmail.com>
2011-08-28 20:16:30 +0200

    Deleted breakpoints

--- classes/kohana/gmaps3.php
+++ classes/kohana/gmaps3.php
@@ -941,10 +941,7 @@ abstract class Kohana_Gmaps3 {
     if (!empty($address))
 		  $url .= '&address='.htmlentities($address);
 		else if (!empty($lat) && !empty($lon))
-		{ 			
-			$url .= '&lat='.$lat.'&lon='.$lon;
-			die($url);	
-		}	
+			$url .= '&lat='.$lat.'&lon='.$lon;	
 		else
 		  throw new Kohana_Exception('You need add address or coordinates params');
 		  
